9bfbaacca61fddf466812fd99525680d835ee34c
Emil Kjelsrud <emil.kjelsrud@intility.no>
2024-03-01 22:12:48 +0100

    fix(login): remove buggy stdin implementation

--- cmd/login.go
+++ cmd/login.go
@@ -12,6 +12,7 @@ const (
 )
 
 var (
+	apiKeyFlag          string
 	endpointFlag        string
 	apiVersionFlag      string
 	modelDeploymentFlag string
@@ -23,31 +24,19 @@ var loginCmd = &cobra.Command{
 	Short: "Authenticate with Azure OpenAI",
 	Long: `Login will prompt you to enter your Azure OpenAI API key and other relevant information required for authentication. 
 Your credentials will be stored securely in your keyring and will never be exposed on the file system directly.
-You can also pass the API key through stdin by using the '-' argument making the command non-interactive. For example:
-
-> echo $API_KEY | cwc login -e "https://my-deployment.openai.azure.com/" -v "2023-12-01-preview" -m "gpt-4-turbo" -
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// if the first argument is '-' or is empty, then we should read from stdin
-		var apiKey string
-		if len(args) == 0 {
-			ui.PrintMessage("Enter your Azure OpenAI API Key: ", ui.MessageTypeInfo)
-			apiKey = config.SanitizeInput(ui.ReadUserInput())
-		} else if args[0] == "-" {
-			reader := cmd.InOrStdin()
-			var buffer []byte
-			buffer = make([]byte, 1024)
-			n, err := reader.Read(buffer)
-			if err != nil {
-				return err
-			}
-			apiKey = string(buffer[:n])
-		}
 
-		// Prompt for other required authentication details (endpoint, version, and deployment)
+		// Prompt for other required authentication details (apiKey, endpoint, version, and deployment)
+		apiKey := apiKeyFlag
 		endpoint := endpointFlag
 		apiVersion := apiVersionFlag
 		modelDeployment := modelDeploymentFlag
+
+		if apiKeyFlag == "" {
+			ui.PrintMessage("Enter the Azure OpenAI API Key: ", ui.MessageTypeInfo)
+			apiKey = config.SanitizeInput(ui.ReadUserInput())
+		}
 
 		if endpointFlag == "" {
 			ui.PrintMessage("Enter the Azure OpenAI API Endpoint: ", ui.MessageTypeInfo)
@@ -87,6 +76,7 @@ You can also pass the API key through stdin by using the '-' argument making the
 func init() {
 
 	// Add flags to the login command
+	loginCmd.Flags().StringVarP(&apiKeyFlag, "api-key", "k", "", "Azure OpenAI API Key")
 	loginCmd.Flags().StringVarP(&endpointFlag, "endpoint", "e", "", "Azure OpenAI API Endpoint")
 	loginCmd.Flags().StringVarP(&apiVersionFlag, "api-version", "v", "", "Azure OpenAI API Version")
 	loginCmd.Flags().StringVarP(&modelDeploymentFlag, "model-deployment", "m", "", "Azure OpenAI Model Deployment")
