c16bb658c20479d665238e513a29e24beea526a1
nkzk <nkzk95@gmail.com>
2024-03-12 03:13:29 +0100

    feat(cwc): add openai as supported LLM provider

--- cmd/login.go
+++ cmd/login.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/emilkje/cwc/pkg/config"
 	"github.com/emilkje/cwc/pkg/errors"
 	"github.com/emilkje/cwc/pkg/ui"
@@ -12,14 +14,16 @@ var (
 	endpointFlag        string
 	apiVersionFlag      string
 	modelDeploymentFlag string
+	modelFlag           string
+	providerFlag        string
 )
 
 // Declaration of a new cobra command for login
 var loginCmd = &cobra.Command{
 	Use:   "login",
-	Short: "Authenticate with Azure OpenAI",
-	Long: `Login will prompt you to enter your Azure OpenAI API key and other relevant information required for authentication. 
-Your credentials will be stored securely in your keyring and will never be exposed on the file system directly.
+	Short: "Authenticate with LLM provider",
+	Long: `Login will prompt you to enter your provider name, API key and other relevant information required for authentication.
+Your credentials will be stored securely in your keyring and will never be exposed on the file system directly. 
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
@@ -28,28 +32,43 @@ Your credentials will be stored securely in your keyring and will never be expos
 		endpoint := endpointFlag
 		apiVersion := apiVersionFlag
 		modelDeployment := modelDeploymentFlag
+		model := modelFlag
+		provider := strings.ToLower(providerFlag)
+
+		if provider == "" {
+			ui.PrintMessage("Enter Provider name: ", ui.MessageTypeInfo)
+			provider = config.SanitizeInput(ui.ReadUserInput())
+		}
 
 		if apiKeyFlag == "" {
-			ui.PrintMessage("Enter the Azure OpenAI API Key: ", ui.MessageTypeInfo)
+			ui.PrintMessage("Enter the API Key: ", ui.MessageTypeInfo)
 			apiKey = config.SanitizeInput(ui.ReadUserInput())
 		}
 
 		if endpointFlag == "" {
-			ui.PrintMessage("Enter the Azure OpenAI API Endpoint: ", ui.MessageTypeInfo)
+			ui.PrintMessage("Enter the API Endpoint: ", ui.MessageTypeInfo)
 			endpoint = config.SanitizeInput(ui.ReadUserInput())
 		}
 
 		if apiVersionFlag == "" {
-			ui.PrintMessage("Enter the Azure OpenAI API Version: ", ui.MessageTypeInfo)
+			ui.PrintMessage("Enter the API Version: ", ui.MessageTypeInfo)
 			apiVersion = config.SanitizeInput(ui.ReadUserInput())
 		}
-
-		if modelDeploymentFlag == "" {
-			ui.PrintMessage("Enter the Azure OpenAI Model Deployment: ", ui.MessageTypeInfo)
-			modelDeployment = config.SanitizeInput(ui.ReadUserInput())
+		if provider == "azure" {
+			if modelDeploymentFlag == "" {
+				ui.PrintMessage("Enter the Azure OpenAI Model Deployment: ", ui.MessageTypeInfo)
+				modelDeployment = config.SanitizeInput(ui.ReadUserInput())
+			}
 		}
 
-		cfg := config.NewConfig(endpoint, apiVersion, modelDeployment)
+		if provider == "openai" {
+			if modelFlag == "" {
+				ui.PrintMessage("Enter the Model name: ", ui.MessageTypeInfo)
+				model = config.SanitizeInput(ui.ReadUserInput())
+			}
+		}
+
+		cfg := config.NewConfig(provider, endpoint, apiVersion, modelDeployment, model)
 		cfg.SetAPIKey(apiKey)
 
 		err := config.SaveConfig(cfg)
@@ -72,8 +91,10 @@ Your credentials will be stored securely in your keyring and will never be expos
 func init() {
 
 	// Add flags to the login command
-	loginCmd.Flags().StringVarP(&apiKeyFlag, "api-key", "k", "", "Azure OpenAI API Key")
-	loginCmd.Flags().StringVarP(&endpointFlag, "endpoint", "e", "", "Azure OpenAI API Endpoint")
-	loginCmd.Flags().StringVarP(&apiVersionFlag, "api-version", "v", "", "Azure OpenAI API Version")
-	loginCmd.Flags().StringVarP(&modelDeploymentFlag, "model-deployment", "m", "", "Azure OpenAI Model Deployment")
+	loginCmd.Flags().StringVarP(&providerFlag, "provider", "p", "", "Provider name. Supported providers: "+strings.Join(config.SupportedProviders, " "))
+	loginCmd.Flags().StringVarP(&modelFlag, "model", "m", "", "OpenAI (compatible) Model")
+	loginCmd.Flags().StringVarP(&apiKeyFlag, "api-key", "k", "", "API Key")
+	loginCmd.Flags().StringVarP(&endpointFlag, "endpoint", "e", "", "API Endpoint")
+	loginCmd.Flags().StringVarP(&apiVersionFlag, "api-version", "v", "", "API Version")
+	loginCmd.Flags().StringVarP(&modelDeploymentFlag, "model-deployment", "d", "", "Azure OpenAI Model Deployment")
 }
